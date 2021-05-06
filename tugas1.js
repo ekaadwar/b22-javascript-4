8cedf02bd4c545d67921092898d38941ac4dacac
Eka Fajhari Adwar <eka.adwar@gmail.com>
2021-05-06 20:48:59 +0700

    clear tugas 1.js

---
+++ tugas1.js
@@ -0,0 +1,23 @@
+let teks = "MalAm";
+let teksArray = teks.split("");
+let length = teksArray.length;
+
+let invers = teksArray[length - 1];
+length -= 2;
+
+for (let i = length; i >= 0; i--) {
+  invers += `${teksArray[i]}`;
+}
+
+function toLowerCase(str) {
+  return str.toLowerCase();
+}
+
+let teksLower = toLowerCase(teks);
+let inversLower = toLowerCase(invers);
+
+if (teksLower == inversLower) {
+  console.log("Palindrom");
+} else {
+  console.log(`Invers = ${invers}`);
+}
